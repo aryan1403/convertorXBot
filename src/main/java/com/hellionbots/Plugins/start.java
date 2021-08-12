d43f68e960864e0b855429c11908cedbf9c9ea6d
Aaryan <aaryan14032006@gmail.com>
2021-08-12 14:07:12 +0530

    Updated

--- src/main/java/com/hellionbots/Plugins/start.java
+++ src/main/java/com/hellionbots/Plugins/start.java
@@ -2,22 +2,20 @@ package com.hellionbots.Plugins;
 
 import com.hellionbots.Master;
 import com.hellionbots.convertor;
-
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-public class start extends convertor implements Master{
+public class start extends convertor implements Master {
 
     @Override
     public void handleRequests(Update update, String cmd) {
-        if(cmd.equalsIgnoreCase(getHandler()+"start")){
-            sendMessage(update, "Hello "
-                + update.getMessage().getFrom().getFirstName()
-                + "\nWelcome to ConvertorXBot I will help you convert "
-                + "text, Files, Document's & Images into Other extensions.\n"
-                + "Type /help to see All the available commands."
-            );
+        if (cmd.equalsIgnoreCase(getHandler() + "start")) {
+            sendMessage(update,
+                "Hello " + update.getMessage().getFrom().getFirstName()
+                    + "\nWelcome to ConvertorXBot I will help you convert "
+                    + "text, Files, Document's & Images into Other extensions.\n"
+                    + "Type /help to see All the available commands.");
+
         }
-        
+
     }
-    
+
 }
